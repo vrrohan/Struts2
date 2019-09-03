393dba145914b708af1211b15f4a5b37690e20bd
rohan.yadav <rohan.yadav@tavant.com>
2019-09-03 19:09:04 +0530

    working with basics of struts2 action classes

---
+++ 04Struts2CodingActions/src/com/ron/message/model/MessageStore.java
@@ -0,0 +1,19 @@
+package com.ron.message.model;
+
+public class MessageStore {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public MessageStore() {
+		this.message = "Welcome Struts2 Message Store";
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
